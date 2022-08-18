9ca2c45974bf1e598da2ab2b5f49c0146a10047e
MaxVelosport <maximgorbacevich@gmail.com>
2022-08-18 11:26:54 +0300

    Create menu.inc.php

---
+++ menu.inc.php
@@ -0,0 +1,8 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> Первый </a></li>
+                    <li><a href="#"> Второй </a></li>
+                    <li><a href="#"> Третий </a></li>
+                    <li><a href="#"> Четвёртый </a></li>
+                </ul>    
+            </nav>
